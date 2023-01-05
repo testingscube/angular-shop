387c9b8282228cc57c3cda77748062f4937fcf43
testingscube <testingscube@gmail.com>
2023-01-05 09:56:57 +0530

    decleared components as per the structure of the angular shop app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from 'src/header/header.component';
+import { RecipesComponent } from 'src/recipes/recipes.component';
+import { RecipeDetailComponent } from 'src/recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from 'src/recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from 'src/recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from 'src/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from 'src/shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/shopping-list/shopping-list.component.ts
@@ -0,0 +1,4 @@
+import { Component } from "@angular/core";
+
+@Component({selector:"app-shopping-list", templateUrl:"./shopping-list.component.html",styleUrls:["./shopping-list.component.css"]})
+export class ShoppingListComponent{}
