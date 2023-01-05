a9de9b262574b5b4774c103c0c79efa299618093
testingscube <testingscube@gmail.com>
2023-01-06 00:07:24 +0530

    Added header code

--- src/shopping-list/shopping-list.component.ts
+++ src/shopping-list/shopping-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
 
 @Component({selector:"app-shopping-list", templateUrl:"./shopping-list.component.html",styleUrls:["./shopping-list.component.css"]})
-export class ShoppingListComponent{}
+export class ShoppingListComponent{
+    
+}
